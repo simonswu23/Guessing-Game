e67c9990bfda14e4494f277786cba1c86ddc4458
Simon Wu <79566257+simonswu23@users.noreply.github.com>
2022-02-23 15:17:27 -0800

    Update QuestionsGame.java

--- QuestionsGame.java
+++ QuestionsGame.java
@@ -1,6 +1,3 @@
-//Simon Wu
-//12/02/2021
-//CSE 143 Section AP, Neel Jog
 //20Questions
 //Creates class QuestionMain which prompts the user to play
 //the 20 questions game that can also be generated from
